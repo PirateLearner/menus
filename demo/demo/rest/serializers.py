bcb0866bb667a8541b4d934be150fec87bf15117
Anshul Thakur <anshulthakur@rediffmail.com>
2019-06-07 19:10:24 +0530

    Initial commit. Project initialized. Some barebones serializers and folder heirarchies initialized.

---
+++ demo/demo/rest/serializers.py
@@ -0,0 +1,23 @@
+'''
+Created on 07-June-2019
+
+@author: anshul
+
+@brief All serializers that do not belong to a specific app but must exist to 
+support that app reside here. For example, a User serializer is required for
+the blogging app, but the blogging app does not create or manage users. So, the
+user serializer is implemented here.
+'''
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="user-detail")
+    gravatar = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = User
+        fields = ('url', 'id', 'username', 'email', 'is_staff', 'gravatar')
+        
+    def get_gravatar(self, obj):
+        return '#'
